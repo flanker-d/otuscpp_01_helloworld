17babfea09df9aba1810d44e7ae0ad37e3fd2e8f
Artyom Sorokoumov <artyom.sorokoumov@gmail.com>
2017-12-07 00:21:43 +0300

    helloworld update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "lib.h"
 #include <spdlog/spdlog.h>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
   auto logger = spdlog::stdout_logger_mt("console");
   logger->info("version {} was started", version());
+
+  std::cout << "Hello, World!" << std::endl;
+
+  logger->info("finished");
+
   return 0;
 }
